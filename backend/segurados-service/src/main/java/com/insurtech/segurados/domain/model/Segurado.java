16c72028892ff2410d7aec845435a07fed677e86
kau4dev <kauavictordev7@gmail.com>
2026-07-07 13:38:19 -0300

    feat: create Segurado model and repository interface with validation logic

---
+++ backend/segurados-service/src/main/java/com/insurtech/segurados/domain/model/Segurado.java
@@ -0,0 +1,36 @@
+package com.insurtech.segurados.domain.model;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Segurado {
+
+    private UUID id;
+    private TipoPessoa tipoPessoa;
+    private String nomeRazaoSocial;
+    private String cpfCnpj;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento; // se tipoPessoa for PF
+    private String enderecoLogradouro;
+    private String enderecoCidade;
+    private String enderecoUf;
+    private String enderecoCep;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public boolean isPessoaFisica(){
+        return TipoPessoa.PF.equals(tipoPessoa);
+    }
+
+    public void validar(){
+
+        if (isPessoaFisica() && dataNascimento == null){
+            throw new IllegalArgumentException("Data de nascimento obrigatória para Pessoa Física");
+        }
+        if (!isPessoaFisica() && dataNascimento != null){
+            throw new IllegalArgumentException("Data de nascimento não deve ser informada para Pessoa Jurídica");
+        }
+    }
+}
