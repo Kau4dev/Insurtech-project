c3db7bb1a3e2e4158c99c3cacc4847136fb54ff5
kau4dev <kauavictordev7@gmail.com>
2026-07-15 19:04:12 -0300

    feat: add custom exceptions for invalid attributes and mandatory birth date in Segurado validation

---
+++ backend/segurados-service/src/main/java/com/insurtech/segurados/domain/exception/AtributoInvalidoPessoaJuridicaException.java
@@ -0,0 +1,7 @@
+package com.insurtech.segurados.domain.exception;
+
+public class AtributoInvalidoPessoaJuridicaException extends RuntimeException {
+    public AtributoInvalidoPessoaJuridicaException(String message) {
+        super(message);
+    }
+}
