833845f42ff06aa093e1bbd7be0601de60a6b3cf
kau4dev <kauavictordev7@gmail.com>
2026-07-15 09:36:16 -0300

    feat: implement ApoliceController and GlobalExceptionHandler for policy management and error handling

---
+++ backend/apolices-service/src/main/java/com/insurtech/apolices/interfaces/controller/ApoliceController.java
@@ -0,0 +1,69 @@
+package com.insurtech.apolices.interfaces.controller;
+
+import com.insurtech.apolices.application.dto.ApoliceRequestDTO;
+import com.insurtech.apolices.application.dto.ApoliceResponseDTO;
+import com.insurtech.apolices.application.dto.AtualizarStatusApoliceDTO;
+import com.insurtech.apolices.application.dto.PageResponseDTO;
+import com.insurtech.apolices.application.usecase.*;
+import com.insurtech.apolices.domain.exception.StatusNaoSuportadoException;
+import com.insurtech.apolices.domain.model.Status;
+import com.insurtech.apolices.domain.model.TipoSeguro;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/apolices")
+@RequiredArgsConstructor
+public class ApoliceController {
+
+    private final CadastrarApoliceUseCase cadastrarApoliceUseCase;
+    private final BuscarPorIdApoliceUseCase buscarPorIdApoliceUseCase;
+    private final BuscarPorNumeroApoliceUseCase buscarPorNumeroApoliceUseCase;
+    private final ListarApolicesUseCase listarApolicesUseCase;
+    private final AtualizarStatusApoliceUseCase atualizarStatusApoliceUseCase;
+
+
+    @PostMapping
+    public ResponseEntity<ApoliceResponseDTO> cadastrarApolice(@RequestBody @Valid ApoliceRequestDTO dto) {
+        ApoliceResponseDTO apolice = cadastrarApoliceUseCase.executar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apolice);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApoliceResponseDTO> buscarPorId(@PathVariable UUID id) {
+        ApoliceResponseDTO apolice = buscarPorIdApoliceUseCase.executar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(apolice);
+    }
+
+    @GetMapping("/{numeroApolice}")
+    public ResponseEntity<ApoliceResponseDTO> buscarPorNumero(@PathVariable String numeroApolice) {
+        ApoliceResponseDTO apolice = buscarPorNumeroApoliceUseCase.executar(numeroApolice);
+        return ResponseEntity.status(HttpStatus.OK).body(apolice);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<PageResponseDTO<ApoliceResponseDTO>> listarApolices(
+            @RequestParam(required = false) UUID numero,
+            @RequestParam(required = false) Status status,
+            @RequestParam(required = false) TipoSeguro tipoSeguro,
+            @ParameterObject Pageable pageable) {
+        return ResponseEntity.ok(listarApolicesUseCase.executar(numero, status, tipoSeguro, pageable));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApoliceResponseDTO> atualizarStatus(
+            @PathVariable UUID id,
+            @RequestBody @Valid AtualizarStatusApoliceDTO dto) throws StatusNaoSuportadoException {
+        ApoliceResponseDTO apolice = atualizarStatusApoliceUseCase.executar(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(apolice);
+    }
+
+}
---
+++ backend/apolices-service/src/main/java/com/insurtech/apolices/interfaces/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.insurtech.apolices.interfaces.exception;
+
+import com.insurtech.apolices.application.dto.ErrorResponse;
+import com.insurtech.apolices.domain.exception.ApoliceNaoEncontradaException;
+import com.insurtech.apolices.domain.exception.SeguradoNaoEncontradoException;
+import com.insurtech.apolices.domain.exception.StatusNaoSuportadoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SeguradoNaoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleSeguradoNaoEncontrado(SeguradoNaoEncontradoException ex, ServletWebRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(buildError(HttpStatus.NOT_FOUND, ex.getMessage(), request, null));
+
+    }
+
+    @ExceptionHandler(ApoliceNaoEncontradaException.class)
+    public ResponseEntity<ErrorResponse> handleApoliceNaoEncontrada(ApoliceNaoEncontradaException ex, ServletWebRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(buildError(HttpStatus.NOT_FOUND, ex.getMessage(), request, null));
+
+    }
+
+    @ExceptionHandler(StatusNaoSuportadoException.class)
+    public ResponseEntity<ErrorResponse> handleStatusNaoSuportado(StatusNaoSuportadoException ex, ServletWebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(buildError(HttpStatus.BAD_REQUEST, ex.getMessage(), request, null));
+    }
+
+    private ErrorResponse buildError(HttpStatus status,
+                                     String message,
+                                     ServletWebRequest request,
+                                     Map<String, String> errors) {
+        return new ErrorResponse(
+                status.value(),
+                message,
+                request.getRequest().getRequestURI(),
+                errors,
+                LocalDateTime.now()
+        );
+    }
+}
